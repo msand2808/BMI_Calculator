2529cba40124c53344a6ddb1c83812b814b7f0d9
= <=>
2025-11-15 19:08:21 -0600

    added a css file and logic for changing background color

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 function calculateBMI(){
-
+    
     makeTagInvisible();
 
     let height = document.getElementById("height").value;
 
-    if(!parseFloat(height) || height <= 0){
+    if(!parseFloat(height) || height <= 0 || isNaN(height)){
         alert("Type a valid number!!");
         document.getElementById("result").style.display = "none";
+        clearFields();
         return;
     }
     
     let weight = document.getElementById("weight").value;
 
-    if(!parseFloat(weight) || weight <= 0){
+    if(!parseFloat(weight) || weight <= 0 || isNaN(weight)){
         alert("Type a valid value!!");
         document.getElementById("result").style.display = "none";
+        clearFields();
         return;
     }
 
@@ -27,24 +29,49 @@ function calculateBMI(){
     if(bmi < 18.5){
         document.getElementById("title").style.display = "block";
         document.getElementById("msg1").style.display = "block";
-    } else if(bmi >= 18.5 && bmi < 24.9){
+        changeBackgroundColor(1);
+    } else if(bmi >= 18.5 && bmi < 25){
         document.getElementById("title").style.display = "block";
         document.getElementById("msg2").style.display = "block";
+        changeBackgroundColor(2);
     } else if(bmi >= 25 && bmi < 29.9){
         document.getElementById("title").style.display = "block";
         document.getElementById("msg3").style.display = "block";
+        changeBackgroundColor(3);
     } else {
         document.getElementById("title").style.display = "block";
         document.getElementById("msg4").style.display = "block";
+        changeBackgroundColor(4);
     }
+
+
 }
 
 function makeTagInvisible(){
-    console.log("Making tags invisible...");
     document.getElementById("result").style.display = "none";
     document.getElementById("title").style.display = "none";
     document.getElementById("msg1").style.display = "none";
     document.getElementById("msg2").style.display = "none";
     document.getElementById("msg3").style.display = "none";
     document.getElementById("msg4").style.display = "none"; 
+}
+
+function clearFields(){
+    document.getElementById("height").value = "";
+    document.getElementById("weight").value = "";
+    makeTagInvisible();
+}
+
+function changeBackgroundColor(idColor){
+    if(idColor === 1){
+        document.getElementById("changeColor").style.backgroundColor = "rgba(172, 34, 34, 1)";
+    } else if(idColor === 2){
+        document.getElementById("changeColor").style.backgroundColor = "rgba(49, 172, 70, 1)";
+    } else if(idColor === 3){
+        document.getElementById("changeColor").style.backgroundColor = "rgba(211, 162, 55, 1)";
+    } else if(idColor === 4){ 
+        document.getElementById("changeColor").style.backgroundColor = "rgba(199, 28, 28, 1)";
+    } else {
+        document.getElementById("changeColor").style.backgroundColor = "black";
+    }
 }
