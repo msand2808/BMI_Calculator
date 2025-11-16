9202010842a05bd2246180e4991b1e5b56fcb007
= <=>
2025-11-15 18:39:18 -0600

    added logic about messages based in your bmi

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculateBMI(){
 
     let height = document.getElementById("height").value;
 
-    if(!parseFloat(height)){
+    if(!parseFloat(height) || height <= 0){
         alert("Type a valid number!!");
         document.getElementById("result").style.display = "none";
         return;
@@ -12,18 +12,39 @@ function calculateBMI(){
     
     let weight = document.getElementById("weight").value;
 
-    if(!parseFloat(weight)){
+    if(!parseFloat(weight) || weight <= 0){
         alert("Type a valid value!!");
         document.getElementById("result").style.display = "none";
         return;
     }
 
     const bmi = (weight / (height * height)).toFixed(2);
-    
-    document.getElementById("result").textContent = `Your BMI is: ${bmi}`;
+
+    document.getElementById("result").textContent = `Your BMI is: ${bmi}!!`;
     document.getElementById("result").style.display = "block";
+    
+    
+    if(bmi < 18.5){
+        document.getElementById("title").style.display = "block";
+        document.getElementById("msg1").style.display = "block";
+    } else if(bmi >= 18.5 && bmi < 24.9){
+        document.getElementById("title").style.display = "block";
+        document.getElementById("msg2").style.display = "block";
+    } else if(bmi >= 25 && bmi < 29.9){
+        document.getElementById("title").style.display = "block";
+        document.getElementById("msg3").style.display = "block";
+    } else {
+        document.getElementById("title").style.display = "block";
+        document.getElementById("msg4").style.display = "block";
+    }
 }
 
 function makeTagInvisible(){
-    //document.getElementById("msg").style.display = "none";
+    console.log("Making tags invisible...");
+    document.getElementById("result").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("msg1").style.display = "none";
+    document.getElementById("msg2").style.display = "none";
+    document.getElementById("msg3").style.display = "none";
+    document.getElementById("msg4").style.display = "none"; 
 }
